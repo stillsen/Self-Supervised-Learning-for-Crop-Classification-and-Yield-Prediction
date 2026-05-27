36738d323cce7ef9035c519be57e1f65c2b1274c
stillsen <47891921+stillsen@users.noreply.github.com>
2026-05-27 09:58:09 +0200

    Update project title for crop yield prediction study

--- 02_Modeling/01_train_lightly-SSL-VICReg_ConvNext_SCV_Combined-SCV-14_SSL-Loss-together_FT-for-crop-type.py
+++ 02_Modeling/01_train_lightly-SSL-VICReg_ConvNext_SCV_Combined-SCV-14_SSL-Loss-together_FT-for-crop-type.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Part of the self-supervised learning for crop yield prediction study entitled "Self-Supervised Learning Advances Crop Classification and Yield Prediction".
+Part of the self-supervised learning for crop yield prediction study entitled "Self-supervised learning for crop yield prediction across diversified cropping systems".
 This script implements fine-tuning of the VICReg model with ConvNext backbone for crop-specific yield prediction.
 
 The script:
