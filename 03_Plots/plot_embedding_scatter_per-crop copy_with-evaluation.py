893c93158eb025e92290d6c1bb9ab3070ed2eec9
stillsen <47891921+stillsen@users.noreply.github.com>
2026-05-27 09:57:40 +0200

    Revise project title in plot_embedding_scatter script
    Updated project title for clarity and accuracy.

--- 03_Plots/plot_embedding_scatter_per-crop copy_with-evaluation.py
+++ 03_Plots/plot_embedding_scatter_per-crop copy_with-evaluation.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Part of the self-supervised learning for crop yield prediction study entitled "Self-Supervised Learning Advances Crop Classification and Yield Prediction".
+Part of the self-supervised learning for crop yield prediction study entitled "Self-supervised learning for crop yield prediction across diversified cropping systems".
 This script analyzes and visualizes the learned embeddings from the SSL model, evaluating their quality and interpretability.
 
 The script:
